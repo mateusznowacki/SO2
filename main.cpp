b2dd49acd7990452d13fe3ac1214150c7d78862d
matty <mattynovy@outlook.com>
2025-03-27 02:06:04 +0100

    move headers to /headers

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <thread>
 
-#include "waiter.hpp"
-#include "philosopher.hpp"
+#include "headers/waiter.hpp"
+#include "headers/philosopher.hpp"
 
-int main(int argc, char* argv[])
-{
-    if (argc < 3)
-    {
-        std::cerr << "Usage: " << argv[0] << " <number_of_philosophers> <iterations_per_philosopher>" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0]
+                  << " <number_of_philosophers> <iterations_per_philosopher>"
+                  << std::endl;
         return 1;
     }
 
@@ -25,24 +25,21 @@ int main(int argc, char* argv[])
     std::thread* threads = new std::thread[numberOfPhilosophers];
 
     // Initialize and start all philosopher threads
-    for (int i = 0; i < numberOfPhilosophers; i++)
-    {
-        philosophers[i] = new Philosopher(i, numberOfPhilosophers, iterations, &waiter);
+    for (int i = 0; i < numberOfPhilosophers; i++) {
+        philosophers[i] = new Philosopher(i, numberOfPhilosophers,
+                                          iterations, &waiter);
         threads[i] = std::thread(philosopherThreadFunction, philosophers[i]);
     }
 
     // Wait for all threads to finish
-    for (int i = 0; i < numberOfPhilosophers; i++)
-    {
-        if (threads[i].joinable())
-        {
+    for (int i = 0; i < numberOfPhilosophers; i++) {
+        if (threads[i].joinable()) {
             threads[i].join();
         }
     }
 
     // Clean up dynamically allocated memory
-    for (int i = 0; i < numberOfPhilosophers; i++)
-    {
+    for (int i = 0; i < numberOfPhilosophers; i++) {
         delete philosophers[i];
     }
     delete[] philosophers;
