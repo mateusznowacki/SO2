8edad943fc8bf98964e1955fd89fc47487bd2474
lilsker <fsoltysiak02@gmail.com>
2025-06-02 01:11:14 +0200

    linter fixes part 56

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,12 +1,13 @@
-#include "user_manager.h"
-#include "client_handler.h"
-#include "chat_room_manager.h"
-
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 #include <iostream>
 #include <thread>
+
+#include "user_manager.h"
+#include "client_handler.h"
+#include "chat_room_manager.h"
+
 
 #pragma comment(lib, "Ws2_32.lib")
 
